0c9a1be6b77f0ec4f228448ed85596ec55e20402
Sasja Koning <sasjakoning@hotmail.com>
2023-02-07 16:28:28 +0100

    added: api fetch WIP

--- visitekaartje/js/script.js
+++ visitekaartje/js/script.js
@@ -6,19 +6,21 @@ const canvasCharacterRive = new rive.Rive({
   autoplay: true,
   stateMachines: "persona-states",
   artboard: "character-card",
-  fit: rive.Fit.cover,
+  fit: rive.Fit.Cover,
   onLoad: (_) => {
     canvasCharacterRive.resizeDrawingSurfaceToCanvas();
   },
 });
 
 
-const clickArea = document.querySelector(".clickArea");
+const clickArea = document.querySelectorAll(".sasjaCardDetails");
 const cardContent = document.querySelector(".sasjaCardContent");
 
-clickArea.addEventListener("click", () => {
-  console.log("clicked")
-  cardContent.classList.toggle("clicked");
+clickArea.forEach((area) => { 
+  area.addEventListener("click", () => {
+
+    cardContent.classList.toggle("clicked");
+  });
 });
 
 
@@ -38,9 +40,27 @@ clickArea.addEventListener("click", () => {
 // });
 
 
+// fetch data from this link: https://whois.fdnd.nl/api/v1/member?id=cldczhjad16yh0av08jxscp0a and console.log it
+
+const cardFirstName = document.querySelector(".cardName > span:first-of-type");
+const cardLastName = document.querySelector(".cardName > span:last-of-type");
+
+const url = "https://whois.fdnd.nl/api/v1/member?id=cldepio2z3xfw0bw5wqihjf47"
+
+fetch(url).then(response => response.json())
+.then(data => createCard(data))
+.catch(err => console.error(err));
 
 
+// write a function that changes html content based on the parameter
 
+function createCard(data) {
+  console.log(data)
 
+  cardFirstName.insertAdjacentHTML('beforeend', data.member.name);
 
+  cardLastName.insertAdjacentHTML('beforeend', data.member.surname);
 
+  // cardFirstName.innerHTML = data.member.name;
+  // cardLastName.innerHTML = data.member.surname;
+};
