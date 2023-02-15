d5ba31182f267906d8ffd2092281a9f16c04190a
sasjakoning <sasjakoning@hotmail.com>
2023-02-15 21:28:37 +0100

    added: base code for swiping functions

--- spa/js/script.js
+++ spa/js/script.js
@@ -45,3 +45,46 @@ async function getRandomStory() {
         console.log(err);
     }
 }
+
+// Create a function that detects mobile touch swipes and console.log the direction
+
+
+// Create a function that detects mobile touch swipes and console.log the direction
+
+const touchArea = document.querySelector('.touch-area');
+
+touchArea.addEventListener('touchstart', handleTouchStart, false);
+touchArea.addEventListener('touchmove', handleTouchMove, false);
+
+let xDown = null;
+
+function getTouches(evt) {
+    return evt.touches ||             // browser API
+        evt.originalEvent.touches; // jQuery
+}
+
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];
+    xDown = firstTouch.clientX;
+ 
+}
+
+function handleTouchMove(evt) {
+    if (!xDown) {
+        return;
+    }
+
+    let xUp = evt.touches[0].clientX;
+    let xDiff = xDown - xUp;
+
+    if (Math.abs(xDiff) > 0) {
+        if (xDiff > 0) {
+            console.log('left');
+        } else {
+            console.log('right');
+        }
+    }
+
+    xDown = null;
+}
+
