e04a1cd060ae1893814d2db95769bc7006adfd78
Sasja Koning <sasjakoning@hotmail.com>
2023-02-22 09:28:44 +0100

    added: localstorage

--- spa/js/modules/routes.js
+++ spa/js/modules/routes.js
@@ -1,89 +1,108 @@
 import { getRandomStory } from "./api.js";
 import { riveDragonLoad } from "./rive.js";
+import { saveStory } from "./localstorage.js";
+
+let currentStory = null;
 
 function home() {
-    const activeWindow = document.querySelector(".visible.window");
-    console.log(activeWindow);
+  const activeWindow = document.querySelector(".visible.window");
 
+  if (activeWindow) {
     for (let i = 0; i < activeWindow.classList.length; i++) {
-        const className = activeWindow.classList[i];
-        if (className.includes("slide-")) {
+      const className = activeWindow.classList[i];
+      if (className.includes("slide-")) {
+        const animDirection = className.split("-")[2];
+        console.log(animDirection);
 
-            const animDirection = className.split("-")[2];
-            console.log(animDirection)
+        activeWindow.classList.add(`slide-out-${animDirection}`);
 
-            activeWindow.classList.add(`slide-out-${animDirection}`);
-
-            activeWindow.addEventListener( "animationend", () => {
-                activeWindow.classList.remove("visible");
-                activeWindow.classList.remove(`slide-out-${animDirection}`)
-                activeWindow.classList.remove(`slide-in-${animDirection}`)
-            }, { once: true } );
-                
-        }
+        activeWindow.addEventListener(
+          "animationend",
+          () => {
+            activeWindow.classList.remove("visible");
+            activeWindow.classList.remove(`slide-out-${animDirection}`);
+            activeWindow.classList.remove(`slide-in-${animDirection}`);
+          },
+          { once: true }
+        );
+      }
     }
+  }
 }
 
 // Function to open a window by adding the "visible" and "slide-in" classes
 function openWindow(window, direction) {
-    window.classList.add(`visible`, `slide-in-${direction}`);
+  const navToggle = document.querySelector(".nav-toggle");
+
+  window.classList.add(`visible`, `slide-in-${direction}`);
+
+  if (navToggle.checked) {
+    navToggle.checked = false;
+  }
 }
 
 async function story() {
-    const windowStory = document.querySelector(".window-story");
-    
-    // Open the story window
-    openWindow(windowStory, "right");
+  const windowStory = document.querySelector(".window-story");
 
-    // Create a new instance of the "story-loading" element and add it to the window
-    if (document.querySelector("story-loading")) {
-        document.querySelector("story-loading").remove();
-    }
+  // Open the story window
+  openWindow(windowStory, "right");
 
-    const storyLoading = document.createElement("story-loading");
-    windowStory.appendChild(storyLoading);
-  
-    const storyLoadingCanvas = storyLoading.shadowRoot.querySelector(
-      "#canvas-dragon-load"
-    );
-  
-    riveDragonLoad(storyLoadingCanvas);
-  
-    // Get a random story from the API
-    const storyContent = await getRandomStory();
-  
-    // when fetch succeeds, hide the "story-loading" element and add the "story-success" element
-    storyLoading.classList.add("hidden");
-  
-    const storySuccess = document.createElement("story-success");
-    const storySuccessTitle = storySuccess.shadowRoot.querySelector(
-      '[slot="story-success-title"]'
-    );
-    const storySuccessAuthor = storySuccess.shadowRoot.querySelector(
-      '[slot="story-success-author"]'
-    );
-    const storySuccessContent = storySuccess.shadowRoot.querySelector(
-      '[slot="story-success-content"]'
-    );
+  // Create a new instance of the "story-loading" element and add it to the window
+  if (document.querySelector("story-loading")) {
+    document.querySelector("story-loading").remove();
+  }
 
-    storySuccessContent.textContent = storyContent.story;
-    storySuccessTitle.textContent = storyContent.title;
-    storySuccessAuthor.textContent = storyContent.author;
+  const storyLoading = document.createElement("story-loading");
+  windowStory.appendChild(storyLoading);
+
+  const storyLoadingCanvas = storyLoading.shadowRoot.querySelector(
+    "#canvas-dragon-load"
+  );
+
+  riveDragonLoad(storyLoadingCanvas);
+
+  // Get a random story from the API
+  const storyContent = await getRandomStory();
   
-    windowStory.appendChild(storySuccess);
+  currentStory = storyContent;
+
+  // when fetch succeeds, hide the "story-loading" element and add the "story-success" element
+  storyLoading.classList.add("hidden");
+
+  const storySuccess = document.createElement("story-success");
+  const storySuccessTitle = storySuccess.shadowRoot.querySelector(
+    '[slot="story-success-title"]'
+  );
+  const storySuccessAuthor = storySuccess.shadowRoot.querySelector(
+    '[slot="story-success-author"]'
+  );
+  const storySuccessContent = storySuccess.shadowRoot.querySelector(
+    '[slot="story-success-content"]'
+  );
+
+  storySuccessContent.textContent = storyContent.story;
+  storySuccessTitle.textContent = storyContent.title;
+  storySuccessAuthor.textContent = storyContent.author;
+
+  windowStory.appendChild(storySuccess);
+
+  const saveBtn = storySuccess.shadowRoot.querySelector(".save-toggle");
+
+  saveBtn.addEventListener("change", () => {
+    saveStory(saveBtn, currentStory);
+  });
+
 }
 
-
-function saved() { 
+function saved() {
   const windowSaved = document.querySelector(".window-saved");
-    
+
   // Open the story window
   openWindow(windowSaved, "bottom");
 }
 
-
 export default {
-    home,
-    story,
-    saved
-}
+  home,
+  story,
+  saved,
+};
